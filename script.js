c152bdeabfb58921f2385576fb75027d0fe7ece2
leloufadel <leloufadel@gmail.com>
2024-04-04 16:17:05 +0000

    Display a map using leaflet

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 navigator.geolocation.getCurrentPosition(function(position){
    console.log(position)
+   const {latitude } = position.coords
+   const {longitude} = position.coords
+
+  console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+  var map = L.map('map').setView([coords], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([coords]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
 
     } , function(){
         alert(`couldn't get your position`)
