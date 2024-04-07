e0430763f574ec01357b293fa877152d45f63dd1
leloufadel <leloufadel@gmail.com>
2024-04-07 14:54:50 +0000

    Create class constructor

--- script.js
+++ script.js
@@ -12,6 +12,16 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
+class App {
+    constructor(){}
+    _getPosition(){}
+    _loadMap(){}
+    _showForm(){}
+    _toggleElevationField(){}
+    _newWorkout(){}
+}
+
+if(navigator.geolocation)
 navigator.geolocation.getCurrentPosition(function(position){
 //    console.log(position)
    const {latitude} = position.coords
