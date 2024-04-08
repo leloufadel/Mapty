38de393fa101aa748b2917ca14a324aaf36c24d3
leloufadel <leloufadel@gmail.com>
2024-04-08 14:20:19 +0000

    Create a new workout

--- script.js
+++ script.js
@@ -10,54 +10,13 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let speed;
-class Workout {
-   date = new Date();
-    id = (Date.now() + ('').slice(-10));
-
-    constructor(distance, duration, coords){
-    this.distance = distance;
-    this.duration = duration;
-    this.coords = coords;
-    }
-}
-
-class Running extends Workout{ 
-    constructor(distance, duration, coords, cadence){
-
-    super(distance, duration, coords) 
-    this.cadence = cadence;
-    this.pace();
-    }
-    pace(){
-        //min/km
-        this.pace = this.duration / this.distance;
-    }
-    
-}
-class Cycling extends Workout{ 
-        constructor(distance, duration, coords, elevationGain){
-
-    super(distance, duration, elevationGain) 
-    this.elevationGain = elevationGain;
-    this.calcspeed();
-
-    }
-    calcspeed(){
-        //min/km
-        this.speed = this.distance / (this.duration/60);
-        return speed;
-    }
-}
-
-const run1 = new Running(39, -12, 5.2, 24, 178);
-const cycling1 = new Cycling([39, -12], 5.2, 24, 178);
- console.log(run1, cycling1)
+let speed, pace;
 
 /////////////////////////////////////////////////////
 class App {
     #map; 
-    #mapEvent
+    #mapEvent;
+    #workout = [];
     constructor(){
         this._getPosition();
 
@@ -79,7 +38,7 @@ navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function(){
             const {latitude} = position.coords;
             const {longitude} = position.coords;
          const coords = [latitude, longitude]
-           console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+        //    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
          
             this.#map = L.map('map').setView(coords, 13);
          
@@ -106,29 +65,41 @@ navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function(){
     _newWorkout(e) {
         const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp));
         const allPositives = (...inputs) => inputs.every(inp => inp > 0)
+        
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+        const {lat, lng} = this.#mapEvent.latlng;
+        let workout;
+        // Prevent default ;
         e.preventDefault();
-      const type = inputType.value;
-      const distance = +inputDistance.value;
-      const duration = +inputDuration.value;
-
       // if workout running , create running Object
 if(type === 'running'){
     const cadence = +inputCadence.value;
     if(!validInputs(distance, duration, cadence) || !allPositives(distance, duration, cadence))
-     return alert('Inputs have to be a Positive Number')
+     return alert('Inputs have to be a Positive Number');
+
+    workout = new Running( distance, duration, [lat, lng], cadence);
+
 }
       // if workout cycling, create cycling Object
       if(type === 'cycling'){
 
         const elevation = +inputElevation.value;
-        if(!validInputs(distance, duration, elevation) || !allPositives(distance, duration, elevation))
+        if(!validInputs(distance, duration, elevation) || !allPositives(distance, duration))
      return alert('Inputs have to be a Positive Number')
+     workout = new Cycling( distance, duration, [lat, lng], elevation);
 
     }
+    // add new object to workout Array: 
+
+    this.#workout.push(workout);
+
+     console.log(workout);
+
         // clear fields: 
         inputElevation.value = inputCadence.value = inputDistance.value = inputDuration.value = '';
       
-          const {lat, lng} = this.#mapEvent.latlng;
           L.marker([lat, lng]).addTo(this.#map)
           .bindPopup(
           L.popup({
@@ -143,8 +114,54 @@ if(type === 'running'){
           .openPopup();
       
       }
+      renderWorkoutMarker(){
+        
+      }
  }
 
+ class Workout {
+    date = new Date();
+     id = (Date.now() + ('').slice(-10));
+ 
+     constructor(distance, duration, coords){
+     this.distance = distance;
+     this.duration = duration;
+     this.coords = coords;
+     }
+ }
+ 
+ class Running extends Workout{ 
+     constructor(distance, duration, coords, cadence){
+ 
+     super(distance, duration, coords) 
+     this.cadence = cadence;
+     this.pace();
+     }
+     pace(){
+         //min/km
+         this.pace = this.duration / this.distance;
+         return pace;
+     }
+     
+ }
+ class Cycling extends Workout{ 
+         constructor(distance, duration, coords, elevationGain){
+ 
+     super(distance, duration, elevationGain) 
+     this.elevationGain = elevationGain;
+     this.calcspeed();
+ 
+     }
+     calcspeed(){
+         //min/km
+         this.speed = this.distance / (this.duration/60);
+         return speed;
+     }
+ }
+ 
+ const run1 = new Running(39, -12, 5.2, 24, 178);
+ const cycling1 = new Cycling([39, -12], 5.2, 24, 178);
+//   console.log(run1, cycling1)
 const app = new App();
 
 
